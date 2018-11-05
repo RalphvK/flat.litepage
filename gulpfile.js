2595ea4c62839830a62a3f8f58322e0f27022655
Ralph van Kruiselbergen <git@ralphvankruiselbergen.nl>
2018-11-05 22:39:32 +0100

    concat and minify js into single file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 'use strict';
- 
+
+var fs = require('fs');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify');
  
 gulp.task('sass', function () {
   return gulp.src('./css/style.scss')
@@ -12,3 +15,27 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
   gulp.watch('./css/*', ['sass']);
 });
+
+//concat
+gulp.task('concat_js', function() {
+    // get file index 
+    var includes = JSON.parse(fs.readFileSync('./js/index.json')).includes;
+    //An array of files is required for the correct order of contact
+    return gulp.src(includes) //file array need for 
+        .pipe(concat('./scripts.js'))
+        .pipe(minify({
+            ext:{
+                src:'',
+                min:'.min.js'
+            },
+            noSource: true}))
+        .pipe(gulp.dest('./js/'));
+});
+
+//watch
+gulp.task('watch', function () {
+    gulp.watch('./js/partials/*.js', ['concat_js']);
+    gulp.watch('./js/index.json', ['concat_js']);
+});
+
+gulp.task('default', ['watch', 'concat_js']);
--- readme.md
+++ readme.md
@@ -4,6 +4,8 @@ Clone this repository and run NPM install:
 
 ```npm install```
 
+# SASS files
+
 Make your changes and build the CSS file using Gulp:
 
 ```gulp sass```
@@ -15,3 +17,25 @@ or Ruby Sass:
 Watch for changes using Ruby Sass:
 
 ```sass css/style.scss --sourcemap --watch```
+
+# Javascript files
+
+The single javascript file is compiled from the index in the ```js/index.json``` file. By default it looks like this:
+
+```json
+{
+    "includes": [
+        "./js/partials/**/*.js"
+    ]
+}
+```
+
+You can run the build using the following Gulp task:
+
+```gulp concat_js```
+
+Watch for changes using the following Gulp task:
+
+```gulp watch```
+
+Running ```gulp``` will run this task by default.
